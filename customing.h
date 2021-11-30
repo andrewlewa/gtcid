7f248c5475ef1f49ee9726dee27b077001b854d1
andrewlewa <80611059+andrewlewa@users.noreply.github.com>
2021-11-30 15:32:34 +0700

    Add files via upload

---
+++ customing.h
@@ -0,0 +1,13 @@
+#include <string>
+#include "corefunc.h"
+#define byte uint8_t
+#define BYTE uint8_t
+
+//Customing GrowID
+string Growid_acc = "";
+
+//Custome Password By JzuvGTI
+string Pass_acc = "";
+
+//Urutan Nomer ID
+int Urutan_ID = 1;
